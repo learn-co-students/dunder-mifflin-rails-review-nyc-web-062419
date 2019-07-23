81082ee036dd12877fd10ae32d80a4eec833442a
joe lazarz <jlazarz87@gmail.com>
2019-07-23 11:47:17 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,54 @@
 class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+    @employee = Employee.all.select do |employee|
+      employee.dog_id == @dog.id 
+    end
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+    if @dog.valid?
+    @dog.update(dog_params)
+    redirect_to @dog
+    else
+    flash[:message] = @dog.errors.full_messages[0]
+    render :edit
+    end
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.create(dog_params)
+    if @dog.valid?
+    @dog.save
+    redirect_to @dog
+    else
+    flash[:message] = @dog.errors.full_messages[0]
+    render :new
+    end
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id])
+    @dog.destroy!
+    redirect_to dogs_path
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,50 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    if @emplyee.valid?
+    @employee.update(employee_params)
+    else
+    flash[:message] = @employee.errors.full_messages[0]
+    render :edit
+    end
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+    if @employee.valid?
+    @employee.save
+    redirect_to @employee
+    else
+    flash[:message] = @employee.errors.full_messages[0]
+    render :new
+    end
+  end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy!
+    redirect_to employees_path
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+  end
 end
