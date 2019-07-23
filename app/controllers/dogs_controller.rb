727b2bd32bdbf7fc3d6e5eab881059f7b04777d2
Anthony <anthonywan00@gmail.com>
2019-07-23 14:32:15 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,19 @@
 class DogsController < ApplicationController
+
+    def index 
+        @dogs = Dog.all
+    end
+
+    
+    def show
+        @dog = Dog.find(params[:id])
+        @owners = @dog.employees
+    end
+
+    def create
+        @dogs = Dog.joins(:employees).order(name: :desc).uniq
+        render :index
+    end
+    
+
 end
