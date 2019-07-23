1ea98153a5b5004dc947879b3919ed09dc99b1b9
yoseflehrer <yolehrer@gmail.com>
2019-07-23 13:12:55 -0400

    done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,14 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def most_dogs
+        @dogs = Dog.all.sort_by {|dog| dog.employees.length}.reverse
+        render :index
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/dogs/most_dogs', to: "dogs#most_dogs", as: 'most_dogs'
   resources :dogs
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
